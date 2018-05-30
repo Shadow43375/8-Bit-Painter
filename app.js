960d2e43ccb3c723806175c37232e0ed18583414
James S. Bayne <enyab.null@gmail.com>
2018-05-30 03:01:29 -0400

    basic features

---
+++ app.js
@@ -0,0 +1,178 @@
+let numberOfRows = 2,
+    numberOfColumns = numberOfRows;
+let cellDimension = (1/numberOfRows)*100;
+let r = 83,
+    g = 109,
+    b = 89;
+var cellColorOnAlive =  "rgb(" + r + ", " + g + ", " + b + ")"; 
+var colorPicker = document.getElementById('colorPicker');
+var container = document.getElementById('container')
+let averageButton = document.getElementById('averageButton');
+averageButton.addEventListener('click', averageColors);
+
+function update(jscolor) {
+    cellColorOnAlive = '#' + jscolor;
+}
+
+function toggleColor() {
+	if(this.style.backgroundColor  === "white") {
+	  this.style.backgroundColor  = cellColorOnAlive;
+	}
+	else if(this.style.backgroundColor  !== "white") {
+		this.style.backgroundColor = "white";
+	}
+}
+
+var row = [];
+var column = [];
+for(let i=0; i<numberOfRows;i++) {
+	for(let j=0; j<numberOfColumns; j++) {
+		column.push(document.createElement('div'));
+		column[j].classList.add('cell');
+		column[j].style.backgroundColor = "white";
+		column[j].style.height = String(cellDimension) + "%";
+		column[j].style.width = String(cellDimension) + "%";	
+		column[j].onclick = toggleColor.bind(column[j]);
+		container.appendChild(column[j]);
+	}
+	row.push(column);
+	column = [];
+}
+
+function averageColors() {
+let patternVectors = [];
+patternVectors.push([-1, -1]);
+patternVectors.push([0, -1]);
+patternVectors.push([1, -1]);
+patternVectors.push([-1, 0]);
+// patternVectors.push([0, 0]);
+patternVectors.push([1, 0]);
+patternVectors.push([-1, 1]);
+patternVectors.push([0, 1]);
+patternVectors.push([1, 1]);
+
+	
+	let count = 0;
+	let colorArray = [];
+	patternVectors.forEach(function(vector) {
+		colorArray.push(row[1+vector[1]][1+vector[0]].style.backgroundColor);
+	});
+	// console.log(chroma.average(colorArray).hex());
+	row[1][1].style.backgroundColor = chroma.average(colorArray).hex();
+}
+
+document.body.appendChild(container);
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('randomButton').addEventListener('click', function(){
+	randomizeGrid(row, 0.50);
+});
+
+function randomizeGrid(matrix, percentEmpty) {
+    let numberOfRows = matrix.length;
+    let numberOfColumns = matrix[0].length
+
+	if(!percentEmpty) {
+		percentEmpty = 0.5;
+	}
+	let percentFull = 1 - percentEmpty;
+
+	let generateWeighedList = function(list, weight) {
+	    let weighed_list = [];
+	     
+	    // Loop over weights
+	    for (let i = 0; i < weight.length; i++) {
+	        let multiples = weight[i] * 100;
+	         
+	        // Loop over the list of items
+	        for (let j = 0; j < multiples; j++) {
+	            weighed_list.push(list[i]);
+	        }
+	    }
+	     
+	    return weighed_list;
+	};
+	 
+	let list = ['empty', 'nonEmpty'];
+	let weight = [percentEmpty, percentFull];
+	let weighed_list = generateWeighedList(list, weight);
+	 
+	let rand = function(min, max) {
+	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+
+
+	for(let i=0; i<numberOfRows;i++) {
+		for(let j=0; j<numberOfColumns; j++) {
+			let random_num = rand(0, weighed_list.length);
+			if(weighed_list[random_num] === "empty") {
+			 	matrix[i][j].style.backgroundColor = "white";
+			}
+			else if(weighed_list[random_num] === "nonEmpty") {
+				matrix[i][j].style.backgroundColor = chroma.random();
+			}
+		}
+	}	
+}
+
+
+document.getElementById('exportButton').addEventListener('click', function() {
+	 // var win = window.open('data:text/plain;charset=utf-8,Hello%20World', '_blank');
+	 var win = window.open(PNGFromGrid(row), '_blank');
+  	 win.focus();
+})
+
+
+
+// var colorArray = [
+//   ["#fffa6b", "#04ff00"],
+//   ["#fe80e3", "#00feff"]
+// ];
+var colorArray2 = [
+  ["red", "green"],
+  ["yellow", "blue"]
+];
+
+var colorArray3 = [
+ ["rgb(255, 0, 0)", " rgb(255, 0, 0)"],
+ ["rgb(255, 0, 0)", "rgb(255, 0, 0)"]
+]
+
+
+function PNGFromGrid(colorArray) {
+  let numberOfRows = colorArray.length;
+  let numberOfColumns = colorArray[0].length;
+  let cellSize = 200;
+
+  var c = document.getElementById("myCanvas");
+  c.height = numberOfRows*cellSize;
+  c.width = numberOfColumns*cellSize;
+
+  var ctx = c.getContext("2d");
+  for(let i = 0; i<numberOfColumns; i++) {
+    for(let j = 0; j<numberOfRows; j++) {
+      console.log(colorArray[j][i]);
+      ctx.fillStyle = colorArray[j][i].style.backgroundColor;
+      ctx.fillRect(cellSize*i, cellSize*j, cellSize, cellSize); 
+    }
+  }
+
+  c.style.visibility = "hidden";
+
+  // Finally, we get the image data using the .toDataURL() canvas method:
+  console.log(c.toDataURL("image/png")); 
+  return c.toDataURL("image/png");
+}
+
+// row[1][0].style.backgroundColor = "red";
+
